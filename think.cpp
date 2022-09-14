89b9ed242ef96004190da6521f24d5c954ffc53d
Host <98633869+ArtedTorturist@users.noreply.github.com>
2022-09-14 17:46:46 -0400

    moar

--- think.cpp
+++ think.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int* dims();
-int vchk(int, string=",");
+int* vchk(int, string=",");
+
 
 int main(){
+}
+
+int* vchk(int nargs, string dem){
 }
